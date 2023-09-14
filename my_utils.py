1308332a124a214a9a6155c4226bc9dbfb6fb95e
sanfordbaran <106628130+kgnu-strategic-planning@users.noreply.github.com>
2023-09-14 06:45:10 -0600

    Implemented get_column()

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,10 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    forest_fires = []
+    
+    with open(file_name, 'r') as f:
+        for line in f:
+            items = line.strip().split(',')           
+            if items[query_column - 1] == query_value:
+                forest_fires.append(items[result_column - 1])
+    
+    return forest_fires
